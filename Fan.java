a52bc118d5368a8ed7df19b0304b1bd0fbdbd772
abhimanyu901 <167248219+abhimanyu901@users.noreply.github.com>
2024-04-17 12:40:12 +0530

    Add files via upload

---
+++ Fan.java
@@ -0,0 +1,19 @@
+package com.in28minutes.oops;
+
+public class Fan {
+
+	private Boolean isOn;
+
+	public void switchOn(Boolean isOn) {
+		this.isOn = true;
+	}
+
+	public void switchOff(Boolean isOn2) {
+		this.isOn = false;
+	}
+
+	public void changeSpeed() {
+
+	}
+
+}
