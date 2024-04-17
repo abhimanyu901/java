06dc9480b6d66bcf9f9ffa45191b1e485988f15c
abhimanyu901 <167248219+abhimanyu901@users.noreply.github.com>
2024-04-17 12:48:29 +0530

    Add files via upload

---
+++ BiNumberRunner.java
@@ -0,0 +1,15 @@
+package com.in28minutes.oops;
+
+public class BiNumberRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BiNumber numbers = new BiNumber(2, 3);
+		System.out.println(numbers.add());
+		System.out.println(numbers.multiply());
+		// numbers.doubled(2, 3);
+
+
+	}
+
+}
