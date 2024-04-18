72a2d66e8b6cc813aa34ac0614560bbdee40b742
abhimanyu901 <167248219+abhimanyu901@users.noreply.github.com>
2024-04-18 10:40:10 +0530

    Add files via upload

--- BiNumberRunner.java
+++ BiNumberRunner.java
@@ -1,14 +1,17 @@
-package com.in28minutes.oops;
+package com.in28minutes.primitive.datatypes;
 
 public class BiNumberRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		BiNumber numbers = new BiNumber(2, 3);
+		BiNumber numbers = new BiNumber(2,3);
+		// System.out.println(numbers.getNumber1());
 		System.out.println(numbers.add());
+		// System.out.println(numbers.addition());
 		System.out.println(numbers.multiply());
-		// numbers.doubled(2, 3);
-
+		numbers.doubleValue();// Double both numbers
+		System.out.println(numbers.getNumber1()); // give different value above
+		System.out.println(numbers.getNumber2());
+		System.out.println(numbers.addition()); // give different value when execute at different flow
 
 	}
 
